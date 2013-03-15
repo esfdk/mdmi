ee5a86f07db453fdf24095943d7ffe616ec8e149
AkaiBaraue <warhammer_skaven1@hotmail.com>
2013-03-15 16:52:07 +0100

    Added own handin package

---
+++ src/jghandin/Main.java
@@ -0,0 +1,20 @@
+package jghandin;
+
+/**
+ * Derp derp
+ * 
+ * @author Jacob
+ */
+public class Main
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
