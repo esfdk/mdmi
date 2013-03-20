4d11526716af9f7f6086a99b63212da9f942061e
Jakob Melnyk <jakobmelnyk@hotmail.com>
2013-03-20 23:56:05 +0100

    Made a huge leap of progress in APriori and added

--- src/jmhandin/algorithms/APriori.java
+++ src/jmhandin/algorithms/APriori.java
@@ -1,23 +1,78 @@
 package jmhandin.algorithms;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.*;
 
 public class APriori {
 	
-	private HashSet<String> FrequentOneItemSet = new HashSet<String>();
-	private HashSet<HashSet<String>> FrequentTwoItemSet = new HashSet<HashSet<String>>();
-	private HashSet<HashSet<String>> FrequentThreeItemSet = new HashSet<HashSet<String>>();
-	private HashSet<HashSet<String>> FrequentFourItemSet = new HashSet<HashSet<String>>();
-	
-	
-	
-	private static ArrayList<HashSet<HashSet<String>>> ItemSubSets(HashSet<String> itemSet)
+	private static void aprioriAlgorithm(String[][] dataSet, int min_support)
 	{
-		ArrayList<HashSet<HashSet<String>>> listOfSubsets = new ArrayList<HashSet<HashSet<String>>>();
+		HashMap<Integer, List<List<String>>> frequentItemSets= new HashMap<Integer, List<List<String>>>();
 		
+		List<List<String>> convertedDataSet = convertDataSet(dataSet);
 		
-		return listOfSubsets;
+		// Step (1)
+		frequentItemSets.put(1, findFrequentOneItemsets(convertedDataSet));
+		
+		// Step (2)
+		for(int k = 2; frequentItemSets.get(k - 1).size() != 0; k++)
+		{
+			// Step (3)
+			List<List<String>> candidateSet = aprioriGen(frequentItemSets.get(k-1)); 
+			
+			HashMap<List<String>, Integer> candidateTimesSeen = new HashMap<List<String>, Integer>();
+			
+			// Step (4)
+			for(int i = 0; i < convertedDataSet.size(); i++)
+			{
+				// Step (5)
+				List<List<String>> candidateSubsets = subset(candidateSet, convertedDataSet.get(0));
+				
+				// Step (6)
+				for(List<String> candidateSubset : candidateSubsets)
+				{
+					// Step (7)
+					if(candidateTimesSeen.containsKey(candidateSubset))
+					{
+						candidateTimesSeen.put(candidateSubset, candidateTimesSeen.get(candidateSubset) + 1);						
+					}
+					else
+					{
+						candidateTimesSeen.put(candidateSubset, 0);
+					}
+				}
+			}
+			
+			// Step (9)
+			for()
+			{
+				frequentItemSets.put(k, );
+			}
+		}
 	}
 	
+	private static List<List<String>> aprioriGen(List<List<String>> frequentItemSet)
+	{
+		return frequentItemSet;
+	}
+	
+	private static List<List<String>> findFrequentOneItemsets(List<List<String>> dataSet) 
+	{
+		
+		return new ArrayList<List<String>>();
+	}
+	
+	private static boolean hasInfrequentSubset()
+	{
+		return true;
+	}
+	
+	private static List<List<String>> subset(List<List<String>> candidateSet, List<String> tuple)
+	{
+		return candidateSet;
+	}
+	
+	private static List<List<String>> convertDataSet(String[][] dataSet)
+	{
+		return new ArrayList<List<String>>();
+	}
 }
---
+++ src/jmhandin/other/Attributes.java
@@ -0,0 +1,35 @@
+package jmhandin.other;
+
+/**
+ * Enum for the attributes.
+ *  
+ * @author Jakob Melnyk
+ */
+public enum Attributes {
+	USERNAME,
+	AGE,
+	DOB,
+	P_SKILL,
+	UNI_STUDY,
+	OS,
+	P_LANG,
+	ENGLISH,
+	ANIMAL,
+	MOUNTAINS,
+	WINTER,
+	RAND_1_10,
+	RAND_0_1_A,
+	RAND_0_1_B,
+	CANTEEN,
+	COLOUR,
+	NEURAL,
+	SQL,
+	SQL_SERVER,
+	AP,
+	SQRT,
+	HOMETOWN,
+	THERB,
+	SOLAR,
+	SEQUENCE,
+	SEQUENCE_NAME;
+}
