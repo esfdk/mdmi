d78131cd931ae87a3601d075309230e0e89bea66
Niklas Hansen <nikl@itu.dk>
2013-04-24 14:23:13 +0200

    derp

---
+++ src/project/CsvWriter.java
@@ -0,0 +1,46 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CsvWriter
+{
+    /**
+     * Private constructor for static class.
+     */
+    private CsvWriter(){}
+    
+    /**
+     * Write data to .csv file.
+     *
+     * @param data The data to write to the file.
+     * @param fileName The file to write data to.
+     */
+    public static void WriteDataToFile(String[][] data, String fileName)
+    {
+        try
+        {
+            FileWriter writer = new FileWriter(fileName);
+
+            for (String[] dataLine : data)
+            {
+                for (int i = 0; i < dataLine.length; i++))
+                {
+                    writer.append('"' + dataLine[i] + '"');
+                    
+                    if (i < dataLine.length-1)
+                    {
+                        writer.append(',');
+                    }
+                }
+                
+                writer.append('\n');
+                writer.flush();
+            }
+            
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
