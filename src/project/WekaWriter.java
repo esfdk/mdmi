c8de2725ff878cdbe8d21de1542964a9295c5dc5
Niklas Hansen <nikl@itu.dk>
2013-05-08 14:35:48 +0200

    conflict aborted

---
+++ src/project/WekaWriter.java
@@ -0,0 +1,23 @@
+package project;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WekaWriter
+{
+    /**
+     * Private constructor for static class.
+     */
+    private WekaWriter() {}
+    
+    /**
+     * Write data to .arff file.
+     *
+     * @param data The data to write to the file.
+     * @param fileName The file to write data to.
+     */
+    public static void writeDataToFile(String[][] data, String fileName)
+    {
+        
+    }
+}
