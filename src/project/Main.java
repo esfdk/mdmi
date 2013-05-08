2b22558b54ecd90a096ef1212a59d3e7a9f31a16
Jakob Melnyk <jakobmelnyk@hotmail.com>
2013-05-08 16:14:41 +0200

    Finished flagged

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,6 +1,8 @@
 package project;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -11,28 +13,30 @@ public class Main {
 	    try
 	    {
 	    	System.out.println("Balance");
-	        String[][] balanceOfPaymentsRawData = CSVFileReader.read("../Files/Project/DataSets/Balance_of_payments_annual.csv", false);
+	        String[][] balanceOfPaymentsRawData = CSVFileReader.read("Files/Project/DataSets/Balance_of_payments_annual.csv", false);
 	        System.out.println("UE");
-    		String[][] unemploymentRawData = CSVFileReader.read("../Files/Project/DataSets/Unemployment_rate_annual.csv", false);
+    		String[][] unemploymentRawData = CSVFileReader.read("Files/Project/DataSets/Unemployment_rate_annual.csv", false);
     		System.out.println("GDP");
-    		String[][] GDPRawData = CSVFileReader.read("../Files/Project/DataSets/Euro_per_inhabitant.csv", false);
+    		String[][] GDPRawData = CSVFileReader.read("Files/Project/DataSets/Euro_per_inhabitant.csv", false);
     		System.out.println("Pop");
-    		String[][] populationRawData = CSVFileReader.read("../Files/Project/DataSets/Population_Annual.csv", false);
+    		String[][] populationRawData = CSVFileReader.read("Files/Project/DataSets/Population_Annual.csv", false);
     		
-
-    		/*
     		String[][] balanceOfPaymentsFlagged = getEntriesWithFlags(balanceOfPaymentsRawData, 7);
     		String[][] unemploymentFlagged = getEntriesWithFlags(unemploymentRawData, 6);
+    		String[][] GDPFlagged = getEntriesWithFlags(GDPRawData, 5);
     		String[][] populationFlagged = getEntriesWithFlags(populationRawData, 5);
-    		*/
 
+    		CsvWriter.writeDataToFile(balanceOfPaymentsFlagged, "Files/Project/DataSets/Flagged/balance_flagged.csv");
+    		CsvWriter.writeDataToFile(unemploymentFlagged, "Files/Project/DataSets/Flagged/unemployment_flagged.csv");
+    		CsvWriter.writeDataToFile(GDPFlagged, "Files/Project/DataSets/Flagged/GDP_flagged.csv");
+    		CsvWriter.writeDataToFile(populationFlagged, "Files/Project/DataSets/Flagged/population_flagged.csv");
+    		
     		int[] matchColumns = {0, 1};
     		int[] balanceOfPaymentsColumns = {6};
     		int[] unemplomentColumns = {5};
     		int[] GDPColumns = {4};
     		int[] populationColumns = {4};
-
-
+    		
     		String[][] data = DataSetHelpers.combineDataSets(unemploymentRawData, unemplomentColumns, matchColumns, balanceOfPaymentsRawData, balanceOfPaymentsColumns, matchColumns);
     		int[] columnsToKeep1 = {2, 3};
     		data = DataSetHelpers.combineDataSets(data, columnsToKeep1, matchColumns, GDPRawData, GDPColumns, matchColumns);
@@ -41,7 +45,7 @@ public class Main {
 			
     		data = removeSpaces(data);
     		data = replace(data, "\":\"", "\"?\"");
-
+    		
     		WekaWriter.writeDataToFile(data, "datafile");
 	    }
 		catch (IOException e)
@@ -87,22 +91,6 @@ public class Main {
 		return data;
 	}
 	
-	private static String[][] replaceCommaWithDot(String[][] data)
-	{
-		for(String[] dataLine : data)
-		{
-			for(String dataItem : dataLine)
-			{
-				if(dataItem.contains(","))
-				{
-					dataItem = dataItem.replace(",", ".");
-				}
-			}
-		}
-		
-		return data;
-	}
-	
 	private static String[][] getEntriesWithFlags(String[][] data, int flagColumn)
 	{
 		String[][] temp = new String[data.length][3];
@@ -113,8 +101,28 @@ public class Main {
 			temp[i][2] = data[i][flagColumn];
 		}
 		
+		List<Integer> flagList = new ArrayList<Integer>();
+		for(int i = 0; i < temp.length; i++)
+		{
+			if(!temp[i][2].equals("\"\""))
+			{
+				System.out.println(temp[i][2]);
+				flagList.add(i);
+			}
+			
+		}
 		
+		int flSize = flagList.size();
 		
-		return data;
+		String[][] rarray = new String[flSize][3]; 
+		
+		for(int i = 0; i < flSize; i++)
+		{
+			rarray[i][0] = temp[flagList.get(i)][0];
+			rarray[i][1] = temp[flagList.get(i)][1];
+			rarray[i][2] = temp[flagList.get(i)][2];
+		}
+		
+		return rarray;
 	}
 }
